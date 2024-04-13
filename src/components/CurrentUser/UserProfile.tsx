152b11ce33793ec1e0d14b4dd973c08374a663b0
Akshay Vs <me.akshay.vs@gmail.com>
2024-04-13 13:04:06 +0530

    fix: lint error

--- src/components/CurrentUser/UserProfile.tsx
+++ src/components/CurrentUser/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import userImage from "../../assets/images/defaultUser.png";
 import { Link } from "react-router-dom";
 

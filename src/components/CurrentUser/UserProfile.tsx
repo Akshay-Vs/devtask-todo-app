6c652b6a61db5605df0ad0381083525035303a42
Akshay Vs <me.akshay.vs@gmail.com>
2024-04-13 12:57:04 +0530

    feat(user): added user profile

---
+++ src/components/CurrentUser/UserProfile.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import userImage from "../../assets/images/defaultUser.png";
+import { Link } from "react-router-dom";
+
+const UserProfile = () => {
+	const [avatar, setAvatar] = useState<string | undefined>();
+
+	useEffect(() => {
+		const image = localStorage.getItem("image");
+		setAvatar(image || userImage);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<Link to="/account" className="cursor-pointer">
+			<img
+				src={avatar}
+				className="mx-8 w-[42px] h-[42px] object-cover object-center rounded-full"
+			/>
+		</Link>
+	);
+};
+
+export default UserProfile;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,26 +1,32 @@
 import React, { ReactNode } from "react";
 import Typography from "../Typography";
+import UserProfile from "../CurrentUser/UserProfile";
+import { Link } from "react-router-dom";
 
 interface INavProps {
-  children?: ReactNode | JSX.Element | undefined;
-  style?: string;
-  props?: React.HTMLAttributes<HTMLDivElement>;
-  title?: string;
+	children?: ReactNode | JSX.Element | undefined;
+	style?: string;
+	props?: React.HTMLAttributes<HTMLDivElement>;
+	title?: string;
 }
 const NavBar = ({ title, props, children, style }: INavProps) => {
-  return (
-    <nav
-      className={`h-16 px-8 flex justify-start items-center bg-white ${style}`}
-      {...props}
-    >
-      <Typography>
-        <Typography.Heading variant="h2" style="opacity-80">
-          {title}
-        </Typography.Heading>
-      </Typography>
-      {children}
-    </nav>
-  );
+	return (
+		<nav
+			className={`h-16 px-8 flex justify-between items-center bg-white ${style}`}
+			{...props}
+		>
+			<Link to={"/"}>
+				<Typography>
+					<Typography.Heading variant="h2" style="opacity-80">
+						{title}
+					</Typography.Heading>
+				</Typography>
+			</Link>
+			{children}
+
+			<UserProfile />
+		</nav>
+	);
 };
 
 export default NavBar;
